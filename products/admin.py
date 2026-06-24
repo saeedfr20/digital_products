61b9edee51d84695ad38f77b124caa51737b8e35
saeed <saeed.farjoo.1383@gmail.com>
2026-06-24 23:21:10 +0330

    Complete product app

--- products/admin.py
+++ products/admin.py
@@ -1,24 +1,25 @@
 from django.contrib import admin
 
-from .models import Category,Product,File
+from .models import Category, Product, File
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['parent','title','is_enable','created_time']
-    list_filter = ['is_enable','parent']
+    list_display = ['parent', 'title', 'is_enable', 'created_time']
+    list_filter = ['is_enable', 'parent']
     search_fields = ['title']
 
 
 class FileInlineAdmin(admin.StackedInline):
     model = File
-    fields = ['title','file','is_enable']
+    fields = ['title', 'file_type', 'file', 'is_enable']
     extra = 0
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','is_enable','created_time']
+    list_display = ['title', 'is_enable', 'created_time']
     list_filter = ['is_enable']
     search_fields = ['title']
     filter_horizontal = ['categories']
-    inlines=[FileInlineAdmin]
+    inlines = [FileInlineAdmin]
--- products/models.py
+++ products/models.py
@@ -16,10 +16,8 @@ class Category(models.Model):
         verbose_name = _('Category')
         verbose_name_plural = _('Categories')
 
-
     def __str__(self):
         return self.title
-
 
 
 class Product(models.Model):
@@ -36,10 +34,22 @@ class Product(models.Model):
         verbose_name = _('Product')
         verbose_name_plural = _('Products')
 
+    def __str__(self):
+        return self.title
+
 
 class File(models.Model):
-    product = models.ForeignKey('Product', verbose_name=_('product'), on_delete=models.CASCADE)
+    FILE_AUDIO = 1
+    FILE_VIDEO = 2
+    FILE_PDF = 3
+    FILE_TYPES = (
+        (FILE_AUDIO, _('audio')),
+        (FILE_VIDEO, _('video')),
+        (FILE_PDF, _('pdf')),
+    )
+    product = models.ForeignKey('Product', verbose_name=_('product'), related_name='files', on_delete=models.CASCADE)
     title = models.CharField(_('title'), max_length=50)
+    file_type = models.PositiveSmallIntegerField(_('file type'), choices=FILE_TYPES)
     file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/')
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
@@ -49,3 +59,6 @@ class File(models.Model):
         db_table = 'files'
         verbose_name = _('File')
         verbose_name_plural = _('Files')
+
+    def __str__(self):
+        return self.title
