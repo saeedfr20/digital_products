31c8ca3571972094e278e641a3f06c42cbf5b314
saeed <saeed.farjoo.1383@gmail.com>
2026-06-30 23:04:47 +0330

    Improve users authentication views.

--- users/views.py
+++ users/views.py
@@ -19,8 +19,7 @@ class RegisterView(APIView):
 
         try:
             user = User.objects.get(phone_number=phone_number)
-            # return Response({'detail': 'User already registered!'},
-            #                 status=status.HTTP_400_BAD_REQUEST)
+        
         except User.DoesNotExist:
             user = User.objects.create_user(phone_number=phone_number)
 
